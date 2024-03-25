8dc860f282307f586862d618fd8bca82074deabe
Marouani Mohamed Habib <75361522+Mohamed9820m@users.noreply.github.com>
2024-03-25 14:00:01 +0100

    Update capteurroute.js

--- Routes/capteurroute.js
+++ Routes/capteurroute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { insertData } = require('../controllers/captController');
+
 
 router.post('/insertData/:reference', insertData);
 
